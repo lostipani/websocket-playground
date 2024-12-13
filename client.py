7d785a975f89e243226254861eb657eb84f11f75
Lorenzo Stipani <85617239+lostipani@users.noreply.github.com>
2024-12-13 16:49:58 +0100

    Update client with mean and std

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import json
 import asyncio
+import numpy as np
 from websockets.asyncio.client import connect
 
 
@@ -7,9 +8,10 @@ async def main(data: list):
     async with connect("ws://localhost:12345") as websocket:
         async for message in websocket:
             data.append(json.loads(message)["value"])
-            print(data)
+            arr = np.array(data)
+            print(f"mean: {np.mean(arr)} and std: {np.std(arr)}")
 
 
 if __name__ == "__main__":
-    data = []
+    data = list()
     asyncio.run(main(data))
